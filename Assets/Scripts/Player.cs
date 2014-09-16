70dd36f5835e1930ebe512d59c726957c8215176
noblesseoblige6 <noblesseoblige6@gmail.com>
2014-09-16 21:02:47 +0900

    added comment into the code and changed the player speed

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,11 +2,12 @@
 using System.Collections;
 
 public class Player : MonoBehaviour {
-	public float speed = 5.0f;
+	public float speed = 4.0f;
 	
 	// Update is called once per frame
 	void Update () {
 		float x = Input.GetAxisRaw ("Horizontal");
+		//方向を求める
 		Vector2 direction = new Vector2(x, 0).normalized;
 		rigidbody2D.velocity = direction * speed;
 	}
