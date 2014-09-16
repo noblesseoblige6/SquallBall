58da23a54499484afd03698a8d307e37b7721315
noblesseoblige6 <noblesseoblige6@gmail.com>
2014-09-16 20:59:30 +0900

    Player moves right and left

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float speed = 5.0f;
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxisRaw ("Horizontal");
+		Vector2 direction = new Vector2(x, 0).normalized;
+		rigidbody2D.velocity = direction * speed;
+	}
+}
