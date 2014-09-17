48ed1a812acd84d81bdc21a7b4868ff521901042
noblesseoblige6 <noblesseoblige6@gmail.com>
2014-09-17 21:34:14 +0900

    tmp commit to merge master

---
+++ Assets/Scripts/StartDisplay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartDisplay : MonoBehaviour {
+	public Vector2 bottunPos;
+	// Use this for initialization
+	void Start () {
+		bottunPos.x = Screen.width;
+		bottunPos.y = Screen.height;
+	}
+	
+	// Update is called once per frame
+	void onGUI () {
+		if (GUI.Button (new Rect(bottunPos.x - 30, bottunPos.y - 30, bottunPos.x + 30, bottunPos.y - 60)
+		               , "Start")) {
+			Application.LoadLevel("playMode");		
+		}
+	}
+}
