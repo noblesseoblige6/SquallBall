def360379ef4779abcf2401b024da24a272ad2bb
Yuki Kamada <yuki@wlan1977.u-aizu.ac.jp>
2014-09-17 17:33:52 +0900

    スクリプトからredball の自動生成

---
+++ Assets/MakeRedBall.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MakeRedBall : MonoBehaviour {
+	// // Kamada < prepare for making balls
+	public 	GameObject 		RedBall;
+	int		num		=		0;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		num++;
+
+		if (num == 60) {
+			num = 0;			
+			Instantiate (this.RedBall);
+
+		}
+	}
+
+		
+
+	
+
+
+}
