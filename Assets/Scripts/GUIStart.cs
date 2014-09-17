7ae84228dd12e6baeba05f675de6b21011e90f88
noblesseoblige6 <noblesseoblige6@gmail.com>
2014-09-18 00:02:18 +0900

    added start display

---
+++ Assets/Scripts/GUIStart.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GUIStart : MonoBehaviour {
+	void OnGUI () {
+		GUILayout.BeginArea(new Rect(Screen.width/2.0f - 10, Screen.height/2.0f + 100, 50, 70));
+		if(GUILayout.Button ("Start")) {
+				Application.LoadLevel("SquallBall");		
+		}
+		GUILayout.EndArea();
+
+	}
+}
