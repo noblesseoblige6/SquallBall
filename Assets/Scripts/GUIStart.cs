42e70bd7dee6b6ee1dd5158e07ca034e3a7afb99
noblesseoblige6 <noblesseoblige6@gmail.com>
2014-09-20 17:23:33 +0900

    resolove conflict

--- Assets/Scripts/GUIStart.cs
+++ Assets/Scripts/GUIStart.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 
 public class GUIStart : MonoBehaviour {
-	void OnGUI () {
-		GUILayout.BeginArea(new Rect(Screen.width/2.0f - 10, Screen.height/2.0f + 100, 50, 70));
-		if(GUILayout.Button ("Start")) {
-				Application.LoadLevel("SquallBall");		
-		}
-		GUILayout.EndArea();
+	void Update () {
+		for (int i = 0; i < Input.touchCount; i++) {
+						// タッチ情報を取得する
+						Touch touch = Input.GetTouch (i);
 
+						if (touch.phase == TouchPhase.Began) {
+								Application.LoadLevel ("SquallBall");	
+						}
+				}
 	}
 }
