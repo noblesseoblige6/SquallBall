0262873b771884685c41278728b8d84ea3010fb7
noblesseoblige6 <noblesseoblige6@gmail.com>
2014-09-20 17:36:58 +0900

    when tapping retry, the game restarts

--- Assets/Scripts/GUIStart.cs
+++ Assets/Scripts/GUIStart.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GUIStart : MonoBehaviour {
-	void Update () {
-		for (int i = 0; i < Input.touchCount; i++) {
-						// タッチ情報を取得する
-						Touch touch = Input.GetTouch (i);
-
-						if (touch.phase == TouchPhase.Began) {
-								Application.LoadLevel ("SquallBall");	
-						}
-				}
+	public class GUIStart : MonoBehaviour {
+		void OnGUI () {
+		if (Event.current.type == EventType.MouseDown) {
+				Application.LoadLevel ("SquallBall");	
+			}
 	}
 }
