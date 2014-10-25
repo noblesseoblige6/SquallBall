da81d0fb70734ecbb4e0c3b9fcf8bbada0462f14
Yuki Kamada <yuki@wlan1263.u-aizu.ac.jp>
2014-10-25 21:13:59 +0900

    latest

--- Assets/Scripts/TestSphere.cs
+++ Assets/Scripts/TestSphere.cs
@@ -54,7 +54,7 @@ public class TestSphere : MonoBehaviour
 
 		touchedPos = (Vector2)Camera.main.ScreenToWorldPoint (Input.mousePosition);
 			//プレイヤーとの距離をチェック
-			if (checkDisPlayer (touchedPos)) 
+		//	if (checkDisPlayer (touchedPos)) 
 
 			/*
 		{
